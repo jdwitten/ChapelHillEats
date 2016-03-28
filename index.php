224e6fa5634c053e31bd378eafa851860603d10e
jdwitten <jdwitten11@gmail.com>
2016-03-28 12:05:29 -0400

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
     $host = "us-cdbr-azure-east-a.cloudapp.net";
     $user = "b2829da0080961";
     $pwd = "926e92e0";
-    $db = "Test";
+    $db = "acsm_2fd6830ca4dfb48";
     $connection = "Database=acsm_2fd6830ca4dfb48;Data Source=us-cdbr-azure-east-a.cloudapp.net;User Id=b2829da0080961;Password=926e92e0";
     // Connect to database.
     try {
