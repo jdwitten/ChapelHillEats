3f12804d5da9fcc06f5ca8db40885d7efd01c4c8
jdwitten <jdwitten11@gmail.com>
2016-05-06 15:06:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" type="text/css" href="mystyle.css">
 </head>
 <body>
-<img id = "Background" url = "http://cdn2.hubspot.net/hub/299588/file-442773704-jpg/UNC_Chapel_Hill/chapelhill1.jpg">
+<img id = "Background" src = "http://cdn2.hubspot.net/hub/299588/file-442773704-jpg/UNC_Chapel_Hill/chapelhill1.jpg">
 <form action="query.php" method="post">
     <select name="Type" class = "dropdown">
         <option value=1>Asian</option>
