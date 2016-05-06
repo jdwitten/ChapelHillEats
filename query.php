c0f646fe9de261929fdb5ac2e4c28c800dd826b4
jdwitten <jdwitten11@gmail.com>
2016-05-06 14:07:54 -0400

    Update query.php

--- query.php
+++ query.php
@@ -1,8 +1,49 @@
 <html>
+<head>
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
 <body>
-<p>
-  Welcome <?php echo $_POST["Type"]; ?><br>
-  Your email address is: <?php echo $_POST["email"]; ?>
-</p>
+    
+<div id = "Topper"><p>Chapel Hill Eats</p></div>
+
+<form action="query.php" method="post">
+    <select name="Type" class = "dropdown">
+        <option value="asian">Asian</option>
+        <option value="bars">Bars</option>
+        <option value="american">American</option>
+        <option value="southern">Southern</option>
+    </select>
+<input type="submit">
+</form>
+ 
+<?php
+
+$host = "us-cdbr-azure-east-a.cloudapp.net";
+$user = "b2829da0080961";
+$pwd = "926e92e0";
+$db = "acsm_2fd6830ca4dfb48";
+
+// Create connection
+$conn = new mysqli($host, $user, $pwd, $db);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "select rname from acsm_2fd6830ca4dfb48.restaraunts where rtype =".$_POST["Type"];
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>Name</th><th>Phone Number</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["rname"]."</td><td>".$row["phone"]."</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
 </body>
 </html>
