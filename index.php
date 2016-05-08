a72aa1778380668b5006adb7020bb92ea01bddb0
jdwitten <jdwitten11@gmail.com>
2016-05-07 23:07:31 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
 <link rel="stylesheet" type="text/css" href="mystyle.css">
 </head>
 <body>
-<form action="query.php" method="post">
-    <select name="Type" class = "dropdown">
-        <option value=1>Asian</option>
-        <option value=2>Bars</option>
-        <option value=3>American</option>
-        <option value=4>Southern</option>
-    </select>
-<input type="submit">
-</form>
+    <h1>Chapel Hill Eats</h1>
+    <form action="query.php" method="post">
+        <select name="Type" class = "dropdown">
+            <option value=1>Asian</option>
+            <option value=2>Bars</option>
+            <option value=3>American</option>
+            <option value=4>Southern</option>
+        </select>
+    <input type="submit">
+    </form>
 </body>
 </html>
