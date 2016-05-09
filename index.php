1e590e847fe95e588fe92b7299c135ad1de64f65
jdwitten <jdwitten11@gmail.com>
2016-05-09 00:31:02 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,16 @@
     <div id = splash>
         <h1>Chapel Hill Eats</h1>
     </div>
-    <form action="query.php" method="post">
-        <select name="Type" class = "dropdown">
-            <option value=1>Asian</option>
-            <option value=2>Bars</option>
-            <option value=3>American</option>
-            <option value=4>Southern</option>
-        </select>
-    <input type="submit">
+    <div id = main>
+        <form action="query.php" method="post">
+            <select name="Type" class = "dropdown">
+                <option value=1>Asian</option>
+                <option value=2>Bars</option>
+                <option value=3>American</option>
+                <option value=4>Southern</option>
+            </select>
+        <input type="submit">
+    </div>
     </form>
 </body>
 </html>
