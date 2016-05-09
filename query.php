33e65d7a34174e8ed2fdcf94fd2ae66dd726861b
jdwitten <jdwitten11@gmail.com>
2016-05-09 15:14:29 -0400

    Update query.php

--- query.php
+++ query.php
@@ -1,19 +1,4 @@
-<html>
-<head>
-<link rel="stylesheet" type="text/css" href="mystyle.css">
-</head>
-<body>
-    
 
-<form action="query.php" method="post">
-    <select name="Type" class = "dropdown">
-        <option value="1">Asian</option>
-        <option value="2">Bars</option>
-        <option value="3">American</option>
-        <option value="4">Southern</option>
-    </select>
-<input type="submit">
-</form>
  
 <?php
 
