d9a31835ebae64b3e26a9decb96a84cf7336157c
jdwitten <jdwitten11@gmail.com>
2016-05-09 22:32:46 -0400

    Update query.php

--- query.php
+++ query.php
@@ -85,7 +85,7 @@ if(strcmp($_POST["Rating"],"Any Rating")){
 else{
     $ratingEval = "and rating =".$_POST["Rating"];
 }
-echo $typeEval . $priceEval . $locationEval . $ratingEval
+echo $typeEval . $priceEval . $locationEval . $ratingEval;
 $sql = "select rname from acsm_2fd6830ca4dfb48.restaraunts where ".$typeEval . $locationEval . $priceEval . $ratingEval;
 $result = $conn->query($sql);
 
