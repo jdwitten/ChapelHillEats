66df1867a3e9ae4f7eca1497f87f888ed3eb66e4
jdwitten <jdwitten11@gmail.com>
2016-05-11 17:40:37 -0400

    Update query.php

--- query.php
+++ query.php
@@ -84,7 +84,7 @@ else{
 }
 if($prev){$sql = "select rname,phone,address,rating from acsm_2fd6830ca4dfb48.restaraunts where ".$typeEval . $locationEval . $priceEval . $ratingEval;}
 else{$sql = "select rname,phone,address,rating from acsm_2fd6830ca4dfb48.restaraunts";}
-
+echo $sql;
 $result = $conn->query($sql);
 function buildRestaurant($rname, $phone, $address, $rating){
        echo "<div class = 'restaurant'><div class = 'name'>".$rname."</div><div class = 'rating'>".$rating."</div><div class = 'info'>Phone Number: ".$phone."</div><div class = 'info'>Address: ".$address."</div></div>";
