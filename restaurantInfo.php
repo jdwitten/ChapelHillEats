ba921a19e7a5ba878aabf284a95484ec738ba087
jdwitten <jdwitten11@gmail.com>
2016-05-11 23:36:46 -0400

    Update restaurantInfo.php

--- restaurantInfo.php
+++ restaurantInfo.php
@@ -1,9 +1,31 @@
 <html>
+<head>
+  
+</head>
 <body>
 <?php
-$rname = $_GET['rname'];
+$rid = $_GET['rname'];
 echo $rname;
+$host = "us-cdbr-azure-east-a.cloudapp.net";
+$user = "b2829da0080961";
+$pwd = "926e92e0";
+$db = "acsm_2fd6830ca4dfb48";
 
+// Create connection
+$conn = new mysqli($host, $user, $pwd, $db);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+$sql = "select rid,rname,phone,address,rating,photoURL from acsm_2fd6830ca4dfb48.restaraunts where rid = ".$rid;
+$result = $conn->query($sql);
+
+$row = $result->fetch_assoc()){ 
+
+echo "<img src =" .$row['photoURL']."></img>";
 ?>
 
 </body>
