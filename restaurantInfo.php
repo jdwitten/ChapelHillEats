ac5208c7828d1a4a3523a63178267cc8b0b96c44
jdwitten <jdwitten11@gmail.com>
2016-05-14 12:56:28 -0400

    Update restaurantInfo.php

--- restaurantInfo.php
+++ restaurantInfo.php
@@ -37,7 +37,7 @@ echo "<h4>".$row['rname']."</h4>";
 echo "<h1>".$row['address']."  ". $row['phone']."</h1>";
 echo "<h3>Rating: ".$row['rating']."</h3>";
 echo "<h6>Write a review for ".$row['rname']."</h6>";
-echo "<input type='range' name='rangeInput' min='0' max='100' onchange='updateTextInput(this.value);'><input type='text' id='textInput' value=''>";
+echo "<input type='range' name='rangeInput' min='0' max='100' onchange='updateTextInput(this.value);'id = 'ratingSlider'><input type='text' id='textInput' value=''>";
 echo "<textarea id = 'newComment'></textarea><input type ='submit' value='Submit'></form>";
 $fetchComments = "select date, comment from acsm_2fd6830ca4dfb48.comments where rid = ".$rid;
 
