35b9ed2b699b23923272aa4ef5fea901b070da08
jdwitten <jdwitten11@gmail.com>
2016-05-16 15:30:11 -0400

    Update restaurantInfoUpdate.php

--- restaurantInfoUpdate.php
+++ restaurantInfoUpdate.php
@@ -81,8 +81,8 @@ if ($comments->num_rows > 0) {
 $conn->close();
 function buildComment($date, $comment){
   echo "<div class = 'comment'>";
-  echo "<h6 class = 'commDate'>Comment written on ".$date.": </h6>";
-  echo  "<h6 class='comm'>". $comment."</h6></div>";
+  echo "<p>Comment written on ".$date.": </p>";
+  echo  "<p>". $comment."<p></div>";
 }
 ?>
 
